ed21ddea8e2aea90865ab0356b3686c49bedf589
KvSanojKV <120358347+KvSanojKV@users.noreply.github.com>
2026-07-25 10:21:25 +0530

    corrrecyed shell script

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -2,5 +2,5 @@
 sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover, I am a dragon V1 ... RAWR" >> dragon.txt
 grep -i dragon dragon.txt
-run: ls -ltra
-run: cat dragon.txt
+ls -ltra
+cat dragon.txt
